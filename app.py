a8dfde4c06e8630702ce3a699e543910240b5e03
PigoFre <78384268+PigoFre@users.noreply.github.com>
2025-01-16 09:08:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import request
+from flask import Flask
 
+
+
+def index():
+  return render_template('index.html')
+  
+
+
+
+if __name__ == "__main__":
+    # Use environment variables for sensitive keys and set debug to True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    index()
