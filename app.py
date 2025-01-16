bff92a8c5eb2b7cc23da95f800fa43568bc1f4d3
PigoFre <78384268+PigoFre@users.noreply.github.com>
2025-01-16 09:26:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,3 @@ if __name__ == "__main__":
     # Use environment variables for sensitive keys and set debug to True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    index()
